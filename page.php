4f3122520ba037eea4c8ff7ad5909f35656fb623
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2011-10-30 11:46:09 -0400

    Removing Under Construction Modified Page.php to reflect it is no longer under construction and removed corresponding css file

--- page.php
+++ page.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>The Horace Mann Record</title>
-    <link type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/reset.css" rel="stylesheet"/>
-    <link type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/underconstruction.css" rel="stylesheet"/>
-</head>
-
-<body>
-<div id="container">
-		<div id="containerHeader"></div>
-        <div id="page">
-			<center><img src="<?php bloginfo('stylesheet_directory'); ?>/images/underconstruction.png" /></center>
-        </div>
-        <div id="containerFooter"></div>
-</div>
-</body>
-</html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<?php get_header(); ?>
+	<?php if (have_posts()) :?>
+    	<?php while (have_posts()) : the_post();?>
+			<div id="content" class="article">
+    			<h2 class="headline"><?php the_title();?></h2>
+    			<p class="timestamp">Last Updated on <?php the_time('l, F jS, Y g:i a') ?></p>
+                <?php edit_post_link('Edit Article', '<p class="edit">','</p>'); ?>
+                <div class="articleText">
+					<?php the_content('');?>
+                </div>
+        <?php endwhile;?>
+	<?php endif;?>    
+<?php get_sidebar(); ?>  
+<?php get_footer(); ?>
