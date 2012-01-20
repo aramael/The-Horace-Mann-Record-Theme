6458c872ab41be65ea95edaca7c29a6cc4fa0439
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2012-01-19 22:38:12 -0500

    CSS Sprites Used for Logos

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 </div><!--END #content-->
 <div id="sidebar">
-<a href="http://horacemann.org"><center><img src="<?php bloginfo('stylesheet_directory'); ?>/images/sidebarLogo2.png" /></center></a>
+<div id="sidebarLogo"></div>
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("sidebar") ) : ?>
 <?php endif; ?>
 <div class="spacing"></div>
